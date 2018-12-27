4d071a538cd1c4b7e87e038c9ef1845333ff2839
Alexander Hermes <alexander.hermes@alnx.org>
2018-12-28 00:25:41 +0800

    Day 5 outline

---
+++ day5.go
@@ -0,0 +1,11 @@
+package main
+
+func day5() {
+	// read input
+
+	// iterate over runes:
+	// if current does not destroy next; copy rune to output
+	// if current destroys next; advance counter by 1 and continue
+
+	// count number of runes
+}
