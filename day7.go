13cc1b86e754d3faa8b80abcc03fd302b0d65164
Alexander Hermes <alexander.hermes@alnx.org>
2019-01-08 18:12:53 +0800

    Day 7 outline

---
+++ day7.go
@@ -0,0 +1,21 @@
+// day 7 outline
+
+// struct AssemblyStep
+// - ID
+// - after: Array of required Assembly steps
+
+// algo outline
+// - look for
+// - parse each line into Map[ID] -> req steps. Append etc
+//
+// find first step: findCandidates([]).
+//
+//
+
+// findCandidates(executionOrder)
+// - find steps that haven't been executed:
+// - loop through steps
+// -- if already executed, skip
+// -- if prereqs not in executionOrder, skip
+// -- pick earliest in alphabet
+// 
