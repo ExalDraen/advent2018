8867ae9ccff0f76f5be6b89633028711150eddad
Alexander Hermes <alexander.hermes@alnx.org>
2019-01-25 23:06:16 +0800

    Day 9 part 1 solution

--- day9.go
+++ day9.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-const day9Players = 9
-const day9MaxMarble = 25
+const day9Players = 478
+const day9MaxMarble = 71240
 
 // Insert value x into slice v at position i
 // shifting everything up by one (i.e. it's inserted "before" i)
@@ -19,51 +19,54 @@ func Delete(v []int, i int) []int {
 	return append(v[:i], v[i+1:]...)
 }
 
-// Convert a circle index into a regular array index,
-// including -ve indices and wrapping.
-func getCircleIndex(i, n int) int {
-	switch {
-	case i <= n && i > 0:
-		return i
-	case i > n:
-		return i % n
-	case i < 0:
-		return n - (-i % n)
-	}
-	return i
-}
-
 func day9() {
-	for j := 0; j < 50; j++ {
-		fmt.Printf("%v -> %v\n", j, getCircleIndex(j, 9))
-		fmt.Printf("%v -> %v\n", -j, getCircleIndex(-j, 9))
-	}
-	// create slice len = maxMarble+1
 	scores := make(map[int]int)
 	circle := []int{0, 1}
 	cur := 1
 	tar := 0
-	player := 0
+	player := 1
 	for i := 2; i <= day9MaxMarble; i++ {
+		// fmt.Printf("[%v] : %v -- cur: %v\n", player, circle, cur)
 		// players take one turn each
 		player = (i % day9Players)
 
 		// Divisible by 23 -> special case
-		// add 23 to score
-		// 7 counterclockwise -> remove and add to score
+		// - add what's about to be placed to score
+		// - 7 counterclockwise -> remove and add to score
 		// current = 1 clockwise from removed marble
+		// Note that we need to deal with wrapping here as well, if cur < 7
 		if i%23 == 0 {
 			scores[player] += i
-			scores[player] += circle[i-7]
-			circle = Delete(circle, i-7)
-			cur = i - 7 + 1
+			tar = cur - 7
+			// Deal with wrapping around 0
+			if tar < 0 {
+				tar = tar + len(circle)
+			}
+			scores[player] += circle[tar]
+			circle = Delete(circle, tar)
+			cur = tar
+			continue
 		}
+
 		// marble needs to be inserted at position 1 and 2 clockwise
-		// from current marble, i.e. before position 2
-		tar = (cur + 2) % len(circle)
-		circle = Insert(circle, tar, i)
+		// from current marble, i.e. before position 2.
+		// - if the new position wraps over the end of the slice
+		// - special case when the new position would be 1 beyond the current slice
+		if tar = cur + 2; tar == len(circle) {
+			circle = append(circle, i)
+		} else {
+			tar = tar % len(circle)
+			circle = Insert(circle, tar, i)
+		}
 		cur = tar
-		fmt.Printf("[%v] : %v\n", player, circle)
 	}
-	fmt.Printf("Circle: %v\n", circle)
+	//fmt.Printf("Circle: %v\n", circle)
+	fmt.Printf("Scores: %+v\n", scores)
+	max := 0
+	for _, s := range scores {
+		if s > max {
+			max = s
+		}
+	}
+	fmt.Printf("Max score: %+v\n", max)
 }
